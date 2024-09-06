d98f9d916d37463ae3156213b6a0beb0e7f32bfd
Roman Kiselev <roman.kiselev@stjude.org>
2024-09-05 19:08:01 -0500

    Finally made the com-port work! Used libs from https://github.com/theolind/mahm3lib

--- sync_device_32/src/main.c
+++ sync_device_32/src/main.c
@@ -19,17 +19,39 @@ int main(void)
 	// Initialize the board
 	sysclk_init();   // Initialize the system clock
 	board_init();    // Initialize the board (configures default pins)
+	
+	
+	// Enable peripheral clock for PIOA (controls PA8 and PA9) - for UART
+	pmc_enable_periph_clk(ID_PIOA);
+
+	// Disable PIO control for PA8 and PA9, and assign to Peripheral A (UART0)
+	pio_set_peripheral(PIOA, PIO_PERIPH_A, PIO_PA8A_URXD | PIO_PA9A_UTXD);
+
+	// Optional: Enable the pull-up on PA8 and PA9 if not needed
+	pio_pull_up(PIOA, PIO_PA8A_URXD | PIO_PA9A_UTXD, 1);
 
     init_IO();
-    //init_UART();
+	
+	const uart_settings_t settings = {
+		.baud_rate = 115200,
+		.parity = UART_PARITY_NO,
+		.ch_mode = UART_CHMODE_NORMAL
+	};
+    
+	sysclk_enable_peripheral_clock(BOARD_ID_USART);
+	my_uart_init(&settings);
 
     // Notify the host that we are ready
     //UART_tx("Sync device is ready. Firmware version: ");
     //UART_tx(VERSION);
 
 
+	char buf;
 	while (1)
 	{
+		buf = uart_read_char();
+		uart_write_char(buf + 1);
+		delay_ms(100);
 /*		// Set the pin high
 		ioport_set_pin_level(CY2_PIN, true);
 		ioport_set_pin_level(CY3_PIN, false);
