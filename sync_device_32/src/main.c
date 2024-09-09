d47123f64ddea27f9d588603db2624986b7704f3
Roman Kiselev <roman.kiselev@stjude.org>
2024-09-09 15:00:47 -0500

    Rudimentary example of working timer/counter for time delays

--- sync_device_32/src/main.c
+++ sync_device_32/src/main.c
@@ -13,7 +13,7 @@
 // Declaration of function prototypes
 void sd_init_IO(void);
 
-
+void sd_init_timer0(void);
 
 
 // ENTRY POINT
@@ -30,13 +30,14 @@ int main(void)
 	sd_send_string("Sync device is ready. Firmware version: ");
     sd_send_string(VERSION);
 	
+	sd_init_timer0();
 
 
 	pdc_rx_clear_cnt(uart_get_pdc_base(UART));
 	while (1)
 	{
 		delay_ms(100);
-		ioport_toggle_pin_level(CY5_PIN);
+		//ioport_toggle_pin_level(CY5_PIN);
 	}
 }
 
@@ -56,4 +57,37 @@ void sd_init_IO(void)
 	ioport_set_pin_dir(CAMERA_PIN, IOPORT_DIR_OUTPUT);
 	
 	// Enable built-in LED on PB27 (Arduino pin 13)
+}
+
+
+#define DELAY_MS 250
+
+
+void sd_init_timer0(void)
+{
+	sysclk_enable_peripheral_clock(ID_TC0);
+	
+	tc_init(TC0, 0,
+			TC_CMR_TCCLKS_TIMER_CLOCK4 | TC_CMR_WAVSEL_UP_RC);
+	
+	uint32_t rc_value = (sysclk_get_peripheral_hz() / 128 / 1000) * DELAY_MS;
+	tc_write_rc(TC0, 0, rc_value);
+
+    // Enable the interrupt on RC compare
+    tc_enable_interrupt(TC0, 0, TC_IER_CPCS);
+	
+	tc_start(TC0, 0);
+	
+	NVIC_EnableIRQ(TC0_IRQn);
+}
+
+void TC0_Handler(void){
+	// Read the status register to clear the interrupt flag
+	tc_get_status(TC0, 0);
+
+	// Toggle LED
+	ioport_toggle_pin_level(CY5_PIN);
+
+
+	tc_start(TC0, 0);
 }
